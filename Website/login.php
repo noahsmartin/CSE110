448bd936cf6366393c6c8c1a94e6274ca9ecdb85
Stephen Shepherd <slshephe@ucsd.edu>
2014-05-07 21:10:21 -0700

    New files from Gilbert

--- Website/login.php
+++ Website/login.php
@@ -7,14 +7,14 @@
   echo "passhash was: ".$passhashstr."<br />";
 
   // Fetch last session number from "lastsession.log"
-  $lastsession = intval(file_get_contents("/u/acsweb/17/017/gplin/write/cs110x/lastsession.log"),0);
+  $lastsession = intval(file_get_contents("lastsession.log"),0);
   echo "Last session was #".$lastsession."<br />";
   $currsession = $lastsession + 1;
   echo "Current session is #".$currsession."<br />";
 
   if ($currsession % 32 == 0)
   {
-    file_put_contents("/u/acsweb/17/017/gplin/write/cs110x/sessions.log",
+    file_put_contents("sessions.log",
       "----------+--------------------+----------\nSession ID|Username            |Passhash\n----------+--------------------+----------\n",
       FILE_APPEND);
   }
@@ -22,7 +22,7 @@
   echo "attempting to write string:<br />";
   echo sprintf('%010d',$currsession)."|".sprintf('%-20s',$username)."|".sprintf('%010d',$passhash)."<br />";
 
-  $writelen = file_put_contents("/u/acsweb/17/017/gplin/write/cs110x/sessions.log",
+  $writelen = file_put_contents("sessions.log",
     sprintf('%010d',$currsession)."|".sprintf('%-20s',$username)."|".sprintf('%010d',$passhash)."\n",
     FILE_APPEND);
 
@@ -33,7 +33,7 @@
 
   echo "updating last session...";
 
-  $writelen = file_put_contents("/u/acsweb/17/017/gplin/write/cs110x/lastsession.log",
+  $writelen = file_put_contents("lastsession.log",
     sprintf('%d',$currsession));
   if ($writelen != strlen(sprintf('%d',$currsession)))
   {
--- Website/writemenu.php
+++ Website/writemenu.php
@@ -6,15 +6,17 @@
   if (json_decode($newMenuData) != NULL) // Ensure that newMenuData is valid JSON!
   {
     // Pretty print the JSON so we can read it server-side (for debugging).
-    $allTheData = json_decode($newMenuData);
-    $prettyData = json_encode($allTheData,JSON_PRETTY_PRINT);
+    $dataAsLine = json_decode($newMenuData);
+    $prettyData = json_encode($dataAsLine,JSON_PRETTY_PRINT);
     echo "Attempting to write data:<br />";
     echo $prettyData."<br />";
-    $writelen = file_put_contents("/u/acsweb/17/017/gplin/write/cs110x/menus/".$newMenuName.".json",$prettyData);
+    // $writelen = file_put_contents("/u/acsweb/17/017/gplin/write/cs110x/menus/".$newMenuName.".json",$prettyData);
+    /*
     if ($writelen != strlen($prettyData))
     {
       echo "Uh oh! Write length didn't match!<br />";
     }
+    */
   }
   else
   {
