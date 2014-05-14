daa845e650fbce9d5c1bfe1141108dd3837aaf9a
Edgardo Castro <Chots@Edgardos-MacBook-Air-2.local>
2014-05-13 23:20:55 -0700

    CSE-52, login now verifies email stuff, creating account also verifies email/password stuff

--- QuickMenu/LoginViewController.m
+++ QuickMenu/LoginViewController.m
@@ -9,6 +9,13 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailText;
+@property (weak, nonatomic) IBOutlet UITextField *passwordText;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UITextField *createAccountEmailText;
+@property (weak, nonatomic) IBOutlet UITextField *createAccountPasswordText;
+@property (weak, nonatomic) IBOutlet UITextField *passwordConfirmText;
+@property (weak, nonatomic) IBOutlet UIButton *createAccountButton;
 
 @end
 
@@ -25,6 +32,59 @@
 
 - (IBAction)cancel:(id)sender {
     [self dismissViewControllerAnimated:YES completion:^{}];
+}
+- (IBAction)login:(id)sender {
+    
+
+    if([self.passwordText.text isEqualToString:@""] || [self.emailText.text isEqualToString:@""])
+    {
+        UIAlertView *errorAlert = [[UIAlertView alloc]
+                                   initWithTitle:@"Error" message:@"All fields must be filled out" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [errorAlert show];
+    }
+    else if([self validateEmail:self.emailText.text])
+    {
+        // Call api here
+    }
+
+    
+}
+
+
+- (IBAction)createAccount:(id)sender {
+    
+    if([self.createAccountPasswordText.text isEqualToString:self.passwordConfirmText.text] == NO){
+        self.createAccountPasswordText.text = nil;
+        self.passwordConfirmText.text = nil;
+        UIAlertView *errorAlert = [[UIAlertView alloc]
+                                   initWithTitle:@"Error" message:@"Passwords don't match" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [errorAlert show];
+    }
+    else if([self.createAccountPasswordText.text isEqualToString:@""] || [self.passwordConfirmText.text isEqualToString:@""]
+            || [self.createAccountEmailText.text isEqualToString:@""])
+    {
+        UIAlertView *errorAlert = [[UIAlertView alloc]
+                                   initWithTitle:@"Error" message:@"All fields must be filled out" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [errorAlert show];
+    }
+    else if([self validateEmail:self.createAccountEmailText.text])
+    {
+        // Call api here
+    }
+}
+
+-(BOOL)validateEmail:(NSString*) email
+{
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    BOOL b = [emailTest evaluateWithObject:email];
+    if(!b)
+    {
+        UIAlertView *errorAlert = [[UIAlertView alloc]
+                                   initWithTitle:@"Error" message:@"Invalid email address" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [errorAlert show];
+    }
+    return b;
 }
 
 - (IBAction)emailPressed:(id)sender {
