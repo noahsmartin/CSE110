1ae484e07bf4343622e5747dd973ab355d97c5d1
Joanne T Wu <jtwu@acs-vilb6e-mac2.ucsd.edu>
2014-05-27 16:33:22 -0700

    cue 86 prompt for password reset. but wrong password prompt still pops up

--- QuickMenu/LoginViewController.h
+++ QuickMenu/LoginViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LoginViewController : UIViewController <UITextFieldDelegate>
+@interface LoginViewController : UIViewController <UITextFieldDelegate, UIAlertViewDelegate>
 
 @end
