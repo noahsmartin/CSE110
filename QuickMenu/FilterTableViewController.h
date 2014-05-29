a58556630efb19a0c7e82cdb1d261bd86929fb2a
noahsmartin <noahsmartin@yahoo.com>
2014-05-29 16:12:25 -0700

    Added control for filter table

---
+++ QuickMenu/FilterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FilterTableViewController.h
+//  Menyou
+//
+//  Created by Noah Martin on 5/29/14.
+//  Copyright (c) 2014 Noah Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterTableViewController : UITableViewController
+
+@end
